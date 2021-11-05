5b5358980bca03ef2188535bbf415117c0f95d52
OdooConsultant <medconsultantweb@gmail.com>
2021-11-05 11:19:35 +0100

    first commit

---
+++ multiple_discount_for_sale/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': ' Multiple Discount For Sale',
+    "version": "15.0.1.0.0",
+    'category': 'Sales/Sales',
+    'description': """
+Multiple Discounts For Sale 
+=============================================
+
+ This module shows a 2 other level of discount in the <i>sale order</i>.
+    """,
+    "development_status": "Beta",
+    "category": "Sale",
+    'author': 'Odoo Consultant medconsultantweb@gmail.com',
+    'website': 'http://www.weblemon.org',
+    'price': '10.0',
+    'currency': 'USD',
+
+    'depends': ['sale_management', 'sale_margin','account'],
+    'demo': [],
+    'data': [
+        'reports/inherit_sale_report_template.xml'
+    ],
+}
---
+++ multiple_discount_for_sale/models/sale_order.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models, _
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.depends('product_uom_qty', 'discount', 'discount2', 'discount3', 'price_unit',
+                 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        if not self._context.get('change'):
+            for line in self:
+                price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+                price2 = price * (1 - (line.discount2 or 0.0) / 100.0)
+                price3 = price2 * (1 - (line.discount3 or 0.0) / 100.0)
+
+                taxes = line.tax_id.compute_all(price3,
+                                                line.order_id.currency_id,
+                                                line.product_uom_qty,
+                                                product=line.product_id,
+                                                partner=line.order_id.partner_shipping_id
+                                                )
+                line.update({
+                    'price_tax': taxes['total_included'] - taxes['total_excluded'],
+                    'price_total': taxes['total_included'],
+                    'price_subtotal': taxes['total_excluded'],
+                })
+                if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                    line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
+
+
